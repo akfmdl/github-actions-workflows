9ab7ce1912e44528f643e2d5bc9d7c2518d1f6c2
gorani <akfmdl@naver.com>
2025-06-28 15:27:15 +0900

    test

--- scripts/docker-build-push-update.js
+++ scripts/docker-build-push-update.js
@@ -150,14 +150,14 @@ async function buildAndPushDockerImage() {
         console.log(`📤 Docker 이미지 푸시 중: ${fullImageName}`);
         try {
             // const pushCommand = `docker push ${fullImageName}`;
-            // console.log(`📋 Push 명령: ${pushCommand}`);
-            // console.log('📺 Docker 푸시 로그:');
-            // console.log('-'.repeat(60));
+            console.log(`📋 Push 명령: ${pushCommand}`);
+            console.log('📺 Docker 푸시 로그:');
+            console.log('-'.repeat(60));
 
-            // execSync(pushCommand, {
-            //     stdio: 'inherit',
-            //     encoding: 'utf8'
-            // });
+            execSync(pushCommand, {
+                stdio: 'inherit',
+                encoding: 'utf8'
+            });
 
             console.log('-'.repeat(60));
             console.log('✅ Docker 이미지 푸시 완료');
