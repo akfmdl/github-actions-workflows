e136d85e7816756a6c18036cb4eeb47e46c39c4d
gorani <akfmdl@naver.com>
2025-07-08 18:13:51 +0900

    test

--- scripts/docker-build-push-update.js
+++ scripts/docker-build-push-update.js
@@ -124,15 +124,15 @@ async function buildAndPushDockerImage() {
         }
 
         try {
-            const buildCommand = `docker build -f ${DOCKERFILE_PATH} -t ${fullImageName}${buildArgsString} ${BUILD_CONTEXT}`;
-            console.log(`📋 Build 명령: ${buildCommand}`);
-            console.log('📺 Docker 빌드 로그:');
-            console.log('-'.repeat(60));
+            // const buildCommand = `docker build -f ${DOCKERFILE_PATH} -t ${fullImageName}${buildArgsString} ${BUILD_CONTEXT}`;
+            // console.log(`📋 Build 명령: ${buildCommand}`);
+            // console.log('📺 Docker 빌드 로그:');
+            // console.log('-'.repeat(60));
 
-            execSync(buildCommand, {
-                stdio: 'inherit',
-                encoding: 'utf8'
-            });
+            // execSync(buildCommand, {
+            //     stdio: 'inherit',
+            //     encoding: 'utf8'
+            // });
 
             console.log('-'.repeat(60));
             console.log('✅ Docker 이미지 빌드 완료');
@@ -149,15 +149,15 @@ async function buildAndPushDockerImage() {
         // Docker 이미지 푸시
         console.log(`📤 Docker 이미지 푸시 중: ${fullImageName}`);
         try {
-            const pushCommand = `docker push ${fullImageName}`;
-            console.log(`📋 Push 명령: ${pushCommand}`);
-            console.log('📺 Docker 푸시 로그:');
-            console.log('-'.repeat(60));
+            // const pushCommand = `docker push ${fullImageName}`;
+            // console.log(`📋 Push 명령: ${pushCommand}`);
+            // console.log('📺 Docker 푸시 로그:');
+            // console.log('-'.repeat(60));
 
-            execSync(pushCommand, {
-                stdio: 'inherit',
-                encoding: 'utf8'
-            });
+            // execSync(pushCommand, {
+            //     stdio: 'inherit',
+            //     encoding: 'utf8'
+            // });
 
             console.log('-'.repeat(60));
             console.log('✅ Docker 이미지 푸시 완료');
